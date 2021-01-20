98020a2a2eeb6675f8f72478561b70b6ef4d129a
smaity1999 <shuvadipmaity1999@gmail.com>
2021-01-20 21:30:35 +0530

    EDPL-DATATECH

--- CSS/main.js
+++ CSS/main.js
@@ -109,3 +109,31 @@ jQuery(function ($) {
 
 // testamonials
 
+$('.items').slick({
+	centerMode: true,
+	centerPadding: '60px',
+	slidesToShow: 3,
+	autoplay: true,
+	autoplaySpeed: 3500,
+	responsive: [
+	  {
+		breakpoint: 768,
+		settings: {
+		  arrows: false,
+		  centerMode: true,
+		  centerPadding: '40px',
+		  slidesToShow: 3
+		}
+	  },
+	  {
+		breakpoint: 480,
+		settings: {
+		  arrows: false,
+		  centerMode: true,
+		  centerPadding: '40px',
+		  slidesToShow: 2
+		}
+	  }
+	]
+  });
+
